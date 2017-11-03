d8b25d324df3c7089d0f5c4c11880f2cc6c4a1b2
Jubaroo <Jubaroo2@gmail.com>
2017-11-02 21:55:38 -0400

    Added the rest of the kingdoms to flags, banners, and wagons

--- com/wurmonline/server/items/factories/Constants.java
+++ com/wurmonline/server/items/factories/Constants.java
@@ -5,60 +5,93 @@ package com.wurmonline.server.items.factories;
  */
 public class Constants {
     public static final String[] WAGON_LIST= {
-            "model.transports.medium.wagon.jenn", // JK
+            "model.transports.medium.wagon.jenn", // Jenn-Kellon
             "model.transports.medium.wagon.molr", // Mol-Rehan
             "model.transports.medium.wagon.hots", // Horde of the Summoned
-            "model.transports.medium.wagon.zjen", // JK
+            "model.transports.medium.wagon.zjen", // Dragon Kingdom
             "model.transports.medium.wagon.empi", // Empire of Mol-Rehan
             "model.transports.medium.wagon.blac", // Black Legion
             "model.transports.medium.wagon.ebon", // Ebonaura
             "model.transports.medium.wagon.king", // Kingdom of Sol
             "model.transports.medium.wagon.ther", // The Roman Republic
             "model.transports.medium.wagon.valh", // Valhalla Descendants
-            "model.transports.medium.wagon.mace"  // Macedonia
+            "model.transports.medium.wagon.mace", // Macedonia
+            "model.transports.medium.wagon.drea", // Dreadnought
+            "model.transports.medium.wagon.thec", // The Crusaders
+            "model.transports.medium.wagon.pand", // Pandemonium
+            "model.transports.medium.wagon.wurm", // Wurm University
+            "model.transports.medium.wagon.yval"  // Valhalla II
     };
 
     public static final String[] NAMES={
-            "JK", "Mol-Rehan", "Horde of the Summoned", "Dragon Kingdom", "Empire of Mol-Rehan", "Black Legion", "Ebonaura",
-            "Kingdom of Sol", "The Roman Republic", "Valhalla Descendants", "Macedonia"
+            "Jenn-Kellon", "Mol-Rehan", "Horde of the Summoned", "Dragon Kingdom", "Empire of Mol-Rehan", "Black Legion", "Ebonaura",
+            "Kingdom of Sol", "The Roman Republic", "Valhalla Descendants", "Macedonia", "Dreadnought", "The Crusaders",
+            "Pandemonium", "Wurm University", "Valhalla II"
     };
 
     public static final String[] BANNER_LIST= {
-            "model.decoration.banner.jenn", // JK
+            "model.decoration.banner.jenn", // Jenn-Kellon
             "model.decoration.banner.molr", // Mol-Rehan
             "model.decoration.banner.hots", // Horde of the Summoned
-            "model.decoration.banner.zjen",
-            "model.decoration.banner.empi",
-            "model.decoration.banner.blac",
-            "model.decoration.banner.ebon",
-            "model.decoration.banner.king",
-            "model.decoration.banner.ther",
+            "model.decoration.banner.zjen", // Dragon Kingdom
+            "model.decoration.banner.empi", // Empire of Mol-Rehan
+            "model.decoration.banner.blac", // Black Legion
+            "model.decoration.banner.ebon", // Ebonaura
+            "model.decoration.banner.king", // Kingdom of Sol
+            "model.decoration.banner.ther", // The Roman Republic
+            "model.decoration.banner.valh", // Valhalla Descendants
+            "model.decoration.banner.mace", // Macedonia
+            "model.decoration.banner.drea", // Dreadnought
+            "model.decoration.banner.thec", // The Crusaders
+            "model.decoration.banner.pand", // Pandemonium
+            //"", // Wurm University
+            //"" // Valhalla II
     };
     public static final String[] BANNER_TALL_LIST= {
-            "model.decoration.tallbanner.jenn", // JK
+            "model.decoration.tallbanner.jenn", // Jenn-Kellon
             "model.decoration.tallbanner.molr", // Mol-Rehan
             "model.decoration.tallbanner.hots", // Horde of the Summoned
-            "model.decoration.tallbanner.zjen",
-            "",
-            "model.decoration.tallbanner.blac",
-            "",
-            "",
-            "model.decoration.tallbanner.ther",
+            "model.decoration.tallbanner.zjen", // Dragon Kingdom
+            "model.decoration.tallbanner.empi", // Empire of Mol-Rehan
+            "model.decoration.tallbanner.blac", // Black Legion
+            "model.decoration.tallbanner.ebon", // Ebonaura
+            "", // Kingdom of Sol
+            "model.decoration.tallbanner.ther", // The Roman Republic
+            "model.decoration.tallbanner.valh", // Valhalla Descendants
+            "model.decoration.tallbanner.mace", // Macedonia
+            "model.decoration.tallbanner.drea", // Dreadnought
+            "model.decoration.tallbanner.thec", // The Crusaders
+            "model.decoration.tallbanner.pand", // Pandemonium
+            //"", // Wurm University
+            //"" // Valhalla II
     };
     
     public static final String[] FLAG_LIST = {
-            "model.decoration.flag.jenn", // JK
+            "model.decoration.flag.jenn", // Jenn-Kellon
             "model.decoration.flag.molr", // Mol-Rehan
             "model.decoration.flag.hots", // Horde of the Summoned
-            "model.decoration.flag.zjen",
-            "model.decoration.flag.empi",
-            "model.decoration.flag.blac",
-            "model.decoration.flag.ebon",
-            "model.decoration.flag.king",
-            "model.decoration.flag.ther",
-
+            "model.decoration.flag.zjen", // Dragon Kingdom
+            "model.decoration.flag.empi", // Empire of Mol-Rehan
+            "model.decoration.flag.blac", // Black Legion
+            "model.decoration.flag.ebon", // Ebonaura
+            "model.decoration.flag.king", // Kingdom of Sol
+            "model.decoration.flag.ther", // The Roman Republic
+            "model.decoration.flag.valh", // Valhalla Descendants
+            "model.decoration.flag.mace", // Macedonia
+            "model.decoration.flag.drea", // Dreadnought
+            "model.decoration.flag.thec", // The Crusaders
+            "model.decoration.flag.pand", // Pandemonium
+            //"", // Wurm University
+            //"" // Valhalla II
     };
 
+/*
+    public static final String[] TOWER_LIST = {
+            "model.decoration.flag.jenn", // Jenn-Kellon
+            "model.decoration.flag.molr", // Mol-Rehan
+            "model.decoration.flag.hots" // Horde of the Summoned
+    };
+*/
 
     public static byte BIRCHWOOD = (byte)14;
 }
